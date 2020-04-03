672345bd60fc80f736cae88b8991f73c91d6d6e8
krill <lpmaze@gmail.com>
2020-04-03 17:59:18 -0400

    cloudinary service being added, got cloudinary types working

--- src/routes/images/images-router.ts
+++ src/routes/images/images-router.ts
@@ -1,22 +1,23 @@
 import express from 'express'
 import ImagesService from './images-service'
-const cloudinary = require('cloudinary').v2
+// const cloudinary = require('cloudinary').v2
 
 const imagesRouter = express.Router()
 const jsonBodyParser = express.json()
 
 imagesRouter.route('/').get(async (req, res, next) => {
   try {
-    let cloudinaryData = await cloudinary.search
-      .expression('folder:oc/highFlyer')
-      // .sort_by('public_id', 'desc')
-      .execute()
+    // let cloudinaryData = await cloudinary.search
+    //   .expression('folder:oc/highFlyer')
+    //   // .sort_by('public_id', 'desc')
+    //   .execute()
 
     // console.log(cloudinaryData)
-    cloudinaryData = cloudinaryData.resources
+    let imgUrls = await ImagesService.getProjectImageUrls()
     const projects = await ImagesService.getProjects(req.app.get('db'))
     // res.json({ projects, cloudinaryData })
-    res.json({ cloudinaryData })
+    res.json({ imgUrls })
+    // res.json({cloudinaryData})
     next()
   } catch (e) {
     next(e)
--- src/routes/images/images-service.ts
+++ src/routes/images/images-service.ts
@@ -1,12 +1,33 @@
-import Knex from "knex"
+import Knex from 'knex'
+import cloudTypes, { v2 as cloudinary } from 'cloudinary'
 
 const ImagesService = {
-  getProjects(db: Knex){
-    return db
-      .from('projects')
-      .select('*')
+  getProjects(db: Knex) {
+    return db.from('projects').select('*')
+  },
+
+  async getProjectImageUrls(project?: string) {
+    try {
+      console.log('getProjectImageUrls')
+      let cloudinaryData = await cloudinary.search
+        .expression(`folder:oc/highFlyer`)
+        // .sort_by('public_id', 'desc')
+        .execute()
+      
+        console.log(cloudinaryData)
+
+      let imgUrls
+
+      cloudinaryData.forEach((img: cloudTypes.MetadataFieldApiResponse) => {
+        console.log('here')
+        imgUrls.push(img.url)
+      })
+
+      return cloudinaryData
+    } catch (e) {
+      return e
+    }
   }
-  
 }
 
 export default ImagesService
