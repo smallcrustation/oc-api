ecb124d0d73d6813aea5071768450f5d1dc6289d
smallcrustation <lpmaze@gmail.com>
2022-07-15 14:57:23 -0400

    working on deployment

--- src/routes/images/images-router.ts
+++ src/routes/images/images-router.ts
@@ -23,7 +23,7 @@ imagesRouter.route('/project-folders').get(async (req, res, next) => {
 imagesRouter.route('/projects').get(async (req, res, next) => {
   try {
     console.log('======BEFORE getAllProjects=====')
-
+    console.log(req.app.get('db'))
     let projectsList = await ImagesService.getAllProjects(req.app.get('db'))
     console.log('======projectsList=====',projectsList)
     res.status(200).json({ projectsList }).end()
